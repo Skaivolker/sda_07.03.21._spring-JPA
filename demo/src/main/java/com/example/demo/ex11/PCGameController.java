6c62bfe91d2a9d5aa76668736d20625c1352403c
Skaivolker <rmt.codemail@gmail.com>
2021-03-07 13:40:41 +0200

    ex11 updated

--- demo/src/main/java/com/example/demo/ex11/PCGameController.java
+++ demo/src/main/java/com/example/demo/ex11/PCGameController.java
@@ -1,13 +1,16 @@
 package com.example.demo.ex11;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PCGameController {
 
-    @GetMapping("/pg-games")
-    public String home(){
+    @GetMapping("/pc-games")
+    public String home(ModelMap map){
+        map.addAttribute("pcGameForm", new PCGameForm());
+        map.addAttribute("createMessage", "Create PC Game");
         return "pcgame";
     }
 }
