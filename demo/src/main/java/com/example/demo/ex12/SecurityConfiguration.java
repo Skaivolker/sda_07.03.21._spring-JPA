2eeab9ed5b872779127f3df36925fe6976e1c228
Skaivolker <rmt.codemail@gmail.com>
2021-03-13 12:11:12 +0200

    smt about ex12

--- demo/src/main/java/com/example/demo/ex12/SecurityConfiguration.java
+++ demo/src/main/java/com/example/demo/ex12/SecurityConfiguration.java
@@ -1,4 +1,41 @@
 package com.example.demo.ex12;
 
-public class SecurityConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/cars").hasAnyRole("ADMIN", "CARS")
+                .antMatchers(HttpMethod.POST, "/api/cars").authenticated()
+                .antMatchers("/api/users/**").hasRole("USER_ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic()
+                .and()
+                .logout()
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .csrf().ignoringAntMatchers("/api/**");
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}Secret_123").roles("ADMIN", "CARS")
+                .and()
+                .withUser("admin2").password("{noop}Secret_123").roles("USER_ADMIN")
+                .and()
+                .withUser("admin3").password("{noop}Secret_123").roles("CARS");
+    }
+
 }
